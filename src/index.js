2dd894350463c67ab52cdc7112eac1fcbcdbb0db
Caio Alencikas <caio.alencikas@gmail.com>
2020-07-05 13:17:02 -0300

    Nodemon configure and auth controller created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const authController = require('./controllers/auth')
+
+const app = express()
+
+// /auth/sign-in
+// /auth/sign-up
+app.use('/auth', authController)
 
 app.get('/', (req, res) => {
-    return res.json('Api running...');
-});
+    return res.json('Api running...')
+})
 
 app.listen(3001, () => {
-    console.log('Listening on port 3001');
-});
+    console.log('Listening on port 3001')
+})
