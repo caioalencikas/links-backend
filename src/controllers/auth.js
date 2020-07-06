61f5e299246602547ab6a18ec80cdf4bded3ee24
Caio Alencikas <caio.alencikas@gmail.com>
2020-07-05 21:00:09 -0300

    Standard api responses

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -16,15 +16,25 @@ router.get('/sign-in', (req, res) => {
 
 router.get('/sign-up', async (req, res) => {
 
-    const email = 'cacalf16@gmail.com'
-    const password = '123456'
+    const {
+        email,
+        password
+    } = req.body
+
+    const account = await Account.findOne({
+        where: {
+            email
+        }
+    })
+    if (account) return res.jsonBadRequest(null, 'Account already exists')
 
     const hash = bcrypt.hashSync(password, saltRounds)
-    console.log(hash)
+    const newAccount = await Account.create({
+        email,
+        password: hash
+    })
 
-    const result = await Account.create({email, password: hash})
-
-    return res.json(result)
+    return res.jsonOK(newAccount, 'Account created')
 })
 
 module.exports = router
