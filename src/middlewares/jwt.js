cb121b41bacbe5fe7d0fc7592e19167b2254a4a4
Caio Alencikas <caio.alencikas@gmail.com>
2020-07-08 07:49:20 -0300

    Excluded auth paths from JWT.

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -4,6 +4,12 @@ const {
 
 const checkJwt = (req, res, next) => {
 
+    const {url: path} = req
+
+    const excludedPaths = ['/auth/sign-in', '/auth/sign-up']
+    const isExcluded = !!excludedPaths.find(p => p.startsWith(path))
+    if(isExcluded) return next()
+
     let token = req.headers['authorization']
     console.log(req.headers)
     token = token ? token.slice(7, token.length) : null
